70cbbad3dfcf97271a5434360cb25b010bd04f86
Chris <92526998+Hendrix8@users.noreply.github.com>
2022-01-24 23:32:34 +0200

    cars moving

--- CarCrash/main2.py
+++ CarCrash/main2.py
@@ -1,5 +1,6 @@
 import pygame
 import button
+import random
 
 pygame.init()
 
@@ -8,6 +9,7 @@ clock = pygame.time.Clock()
 winWidth = 800
 winHeight = 600
 win = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Car Crash")
 bgImg = pygame.image.load("CarCrash/resources/background.png")
 bg = pygame.transform.scale(bgImg, (800,600))
 i = 0 # background coordinate Y (which is going to change with a loop)
@@ -30,15 +32,40 @@ openBlue = pygame.image.load("CarCrash/resources/myCarOpenBlue.png")
 purple = pygame.image.load("CarCrash/resources/myCarPurple.png")
 red = pygame.image.load("CarCrash/resources/myCarRed.png")
 
+# enemy car images
+car1 = pygame.image.load("CarCrash/resources/car2.png")
+car2 = pygame.image.load("CarCrash/resources/car3.png")
+amb = pygame.image.load("CarCrash/resources/ambulance.png")
+taxi = pygame.image.load("CarCrash/resources/taxi.png")
+
 Cars = [blue, black, coffee, darkBrown, darkGrey, darkPink, lightBlue, lightGrey, openBlue, purple, red]
 carsNumber = len(Cars)
 
 # Changing the sizes of the images of the cars 
-carsResized = [pygame.transform.scale(i, (220,220)) for i in Cars]
+carWidth = 220
+carHeight = 220
+carsResized = [pygame.transform.scale(i, (carWidth,carHeight)) for i in Cars]
 
-#Car coordinates
+# Car coordinates
 x = 230
 y = 380
+
+# Enemy
+enemyX_spawns = [105, 230, 355, 480]
+enemyImg = [car1,car2,amb,taxi]
+enemyX = []
+enemyY = -220
+enemyY_change = 10 #difficulty 
+numOfEnemies = 1
+
+for i in range(numOfEnemies):
+        enemyImgRes = [pygame.transform.scale(i,(carWidth,carHeight)) for i in enemyImg]
+        enemyX.append(random.choice(enemyX_spawns))
+
+
+# Drawing Enemy         
+def enemyDraw(image, x, y):
+        win.blit(image,(x,y))
 
 
 
@@ -47,6 +74,12 @@ startBtn = button.Button(50, 100, startImg, 0.3)
 exitBtn = button.Button(50, 200, exitImg, 0.3)
 colorBtn = button.Button(50,300,changeColorImg, 0.3)
 car = 0     
+
+# Choosing a random enemy to spawn first 
+enemy = random.choice(enemyImgRes) # random enemy car
+enemy_x = random.choice(enemyX_spawns) # random spawn x coordinate
+enemy_y = enemyY # fixed y spawn 
+
 running = True 
 while running:
 
@@ -79,8 +112,19 @@ while running:
 
         i += 5
 
+
         if startBtn.draw(win):
-                win.blit(bg, (0, i))       
+                print("Start")
+                
+        
+        enemyDraw(enemy, enemy_x,enemy_y)
+        enemy_y += enemyY_change #difficulty 
+
+        if enemy_y > winHeight:
+                enemy_y = 0 - carHeight
+                enemy_x = random.choice(enemyX_spawns)
+                enemy = random.choice(enemyImgRes)
+
                                   
         win.blit(selectedCar, (x,y))
 
